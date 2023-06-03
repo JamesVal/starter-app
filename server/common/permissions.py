cc35867bb7989e793c34b7b862c141a05d3b88f3
JamesVal <james.jo.val@gmail.com>
2023-06-03 01:24:54 -0700

    refactor(permission): Added a common permission for most things to follow the same pattern

--- server/common/permissions.py
+++ server/common/permissions.py
@@ -1,5 +1,35 @@
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class IsOwnerPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (obj.owner.user == request.user) or request.user.is_superuser
+
+class CommonPermission(permissions.BasePermission):
+    def get_permissions(self, action):
+        if action == 'list':
+            permission_classes = [AllowAny]
+        elif action == 'retrieve':
+            permission_classes = [AllowAny]
+        elif action == 'create':
+            permission_classes = [IsAuthenticated]
+        elif action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated, IsOwnerPermission]
+        else:
+            permission_classes = [IsAuthenticated]
+
+        return permission_classes
+
+    def has_permission(self, request, view):
+        permission_classes = self.get_permissions(view.action)
+        return all(
+            permission().has_permission(request, view)
+            for permission in permission_classes
+        )
+
+    def has_object_permission(self, request, view, obj):
+        permission_classes = self.get_permissions(view.action)
+        return all(
+            permission().has_object_permission(request, view, obj)
+            for permission in permission_classes
+        )
--- server/item/views.py
+++ server/item/views.py
@@ -3,26 +3,10 @@ from .models import Item
 from .serializers import ItemSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from common.permissions import IsOwnerPermission
+from common.permissions import CommonPermission
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     authentication_classes = [JWTAuthentication, SessionAuthentication, BasicAuthentication]
-
-    def get_permissions(self):
-        permission_classes = [IsAuthenticated]
-
-        if self.action == 'list':
-            permission_classes = [AllowAny]
-        elif self.action == 'retrieve':
-            permission_classes = [AllowAny]
-        elif self.action == 'create':
-            permission_classes = [IsAuthenticated]
-        elif self.action in ['update', 'partial_update', 'destroy']:
-            permission_classes = [IsAuthenticated, IsOwnerPermission]
-        else:
-            permission_classes = [IsAuthenticated]
-
-        return [permission() for permission in permission_classes]
+    permission_classes = [CommonPermission]
