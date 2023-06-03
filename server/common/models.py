48ed10f4eaa5457517ce2c225cf378ec636fab08
JamesVal <james.jo.val@gmail.com>
2023-06-02 20:29:25 -0700

    chore(clean): Removed unused code

--- server/common/models.py
+++ server/common/models.py
@@ -7,13 +7,6 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
     deleted_at = models.DateTimeField(blank=True, null=True)
-    # owner = models.ForeignKey(
-    #     'settings.AUTH_USER_MODEL',
-    #     on_delete=models.SET_NULL,
-    #     blank=True,
-    #     null=True,
-    #     related_name='owner+'
-    # )
 
     class Meta:
         abstract = True
