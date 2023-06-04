72aa334a3c816f00d70df91b29ea7af58d1a42ee
JamesVal <james.jo.val@gmail.com>
2023-06-03 23:33:05 -0700

    feat(comment): Added field validation as a mixin

---
+++ server/common/mixins.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+class RequiredFieldsMixin:
+    required_fields = {}
+
+    def validate_required_fields(self, data):
+        errors = {}
+        for field_name, error_message in self.required_fields.items():
+            if not data.get(field_name):
+                errors[field_name] = error_message
+        if errors:
+            raise serializers.ValidationError(errors)
+        return data
+
+    def validate(self, data):
+        data = super().validate(data)
+        data = self.validate_required_fields(data)
+        return data
--- server/item/models.py
+++ server/item/models.py
@@ -6,7 +6,7 @@ from type.models import Type
 # Create your models here.
 
 class Item(BaseModel):
-    itemName = models.CharField(max_length=255, unique=True)
+    itemName = models.CharField(max_length=255)
     itemDescription = models.TextField(blank=True, null=True)
     type = models.ForeignKey(Type, on_delete=models.CASCADE, blank=True, null=True)
     owner = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True, null=True, related_name="items")
